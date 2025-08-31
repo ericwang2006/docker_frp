82fd1f4876ade25d6d6e5eb6c0b4fb99d7be1cf9
ericwang2006 <erciwang2006@gmail.com>
2025-08-31 14:07:40 +0800

    增加编译镜像工作流

--- docker_frps/buildx.sh
+++ docker_frps/buildx.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Get the latest FRP release version
 latest=$(wget -q -O - "https://api.github.com/repos/fatedier/frp/releases" | jq -r '.[0].tag_name')
-docker buildx build -t ericwang2006/frps -t ericwang2006/frps:$latest --no-cache --platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 . --push
+
+# Build and push multi-architecture images
+docker buildx build \
+  -t ericwang2006/frps:latest \
+  -t ericwang2006/frps:$latest \
+  --no-cache \
+  --platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
+  --push \
+  .
